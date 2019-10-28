8c85027c003131d832760be9b03553057e144b02
Daniel <danielcarr@students.abtech.edu>
2019-10-27 20:24:50 -0400

    original commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="utf-8">
+    <title>Assignment 7 Exercises</title>
+  </head>
+  
+  <body>
+    <h1>Assignment 7 Exercises</h1>
+    <h2>Fixits</h2>
+    <ol>
+      <li><a href="chapter15/fixit1.php">fixit1</a></li>
+      <li><a href="chapter15/fixit2.php">fixit2</a></li>
+      <li><a href="chapter15/fixit3.php">fixit3</a></li>
+      <li><a href="chapter15/fixit4.php">fixit4</a></li>
+      <li><a href="chapter15/fixit5.php">fixit5</a></li>
+    </ol>
+    <h2>Modify</h2>
+    <ol>
+      <li><a href="chapter15/modify1.html">modify1</a></li>
+      <li><a href="chapter15/modify2.html">modify2</a></li>
+      <li><a href="chapter15/modify3.html">modify3</a></li>
+    </ol>
+    <h2>Coding Exercises</h2>
+    <ol>
+      <li><a href="chapter15/paint-estimate.html">Exercise 1</a></li>
+      <li><a href="chapter15/software-order.html">Exercise 2</a></li>
+      <li><a href="chapter15/compare-characters.php">Exercise 3</a></li>
+      <li><a href="chapter15/cube-test.php">Exercise 4</a></li>
+      <li><a href="chapter15/simple-class-apps.php">Exercise 5</a></li>
+  </body>
+  
+</html>
